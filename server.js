55046f8318aa053908d16f699c6fab7911b9ee81
bfflbcm8 <113405317+bfflbcm8@users.noreply.github.com>
2022-10-21 14:15:21 +0700

    make it better

--- server.js
+++ server.js
@@ -1,68 +1,55 @@
 const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-
+const ObjectId = require('mongodb').ObjectId;
 const PORT = process.env.PORT || 8080;
-
 const app = express();
-
 // This tells Express we’re using EJS as the template engine.
 app.set('view engine', 'ejs')
-
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const connectionString = "mongodb+srv://chautest:abc123456@clustertest.onztxwx.mongodb.net/?retryWrites=true&w=majority"
-
+const connectionString = 'mongodb+srv://chaudb:abc123456@cluster13.ynbwogf.mongodb.net/?retryWrites=true&w=majority'
 // server -> connect -> MongoDB
 MongoClient.connect(connectionString, (err, client) => {
-
     if (err) return console.error(err)
-
     console.log('Connected to Database')
-
-    // server -> create -> database -> 'star-wars-quotes' 
-    const db = client.db('CuaHang')
-
-    // server -> create -> collection -> 'quotes'
+    // server -> create -> database -> 'ATNShop' 
+    const db = client.db('ATNShop')
+    // server -> create -> collection -> 'Product'
     const quotesCollection = db.collection('Product')
-
-    // client -> button -> submit -> request -> post -> '/quotes' 
+    // client -> button -> submit -> request -> post -> '/Product' 
     app.post('/Product', (req, res) => {
-
         // server -> insert -> data -> from client 
         quotesCollection.insertOne(req.body)
-            .then(result => {
-                
+            .then(result => {             
                 // server -> result -> console 
-                //console.log(result)
-
                 res.redirect('/')
             })
             .catch(error => console.error(error))
-
     })
-
     // We normally abbreviate `request` to `req` and `response` to `res`.
     // client -> request -> localhost:3000 -> server -> response -> index.html
-    // server -> find -> database -> collection -> quotes -> documents
+    // server -> find -> database -> collection -> Product -> documents
     app.get('/', (req, res) => {
         db.collection('Product').find().toArray()
         .then(results => {
         
             //console.log(results)
             // server -> index.ejs -> client 
-            res.render('index.ejs', { quotes: results })
-        
+            res.render('index.ejs', { quotes: results })      
         })
-        .catch(error => console.error(error))
-        
+        .catch(error => console.error(error))     
     })
-
-
+    // Code delete id, when click button remove in website
+    app.get('/Product/remove/:id', (req, res) => {
+        db.collection('Product').deleteOne({_id: ObjectId(req.params.id)})
+        .then(results => {
+            res.redirect('/');      
+        })
+        .catch(error => console.error(error))     
+    })
     // server -> listen -> port -> PORT
     app.listen(PORT, function() {
         console.log('listening on ' + PORT)
     })
-
 })
